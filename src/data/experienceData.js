5ca9853c7a8e6329d8f3bca106a5e4974c8eff5c
akmuola <akinroyemo@gmail.com>
2024-09-30 17:05:55 +0100

    MyWebsite

---
+++ src/data/experienceData.js
@@ -0,0 +1,44 @@
+const expData = [
+   
+    {
+      id: "1",
+      org: "Aptech Nigeria",
+      title: "Senior Frontend Developer",
+      date: "Mar 2024 - Present",
+      description:  " design wireframes and develop frontend pages from concept to completion. I combine creativity with deep technical expertise to deliver exceptional user experiences. My responsibilities span from initial ideation and brainstorming to the final refinement of the user interface. I work closely with cross-functional teams to ensure seamless collaboration and flawless project execution, driving the frontend development process to meet both user needs and business objectives."
+    },
+    {
+        id: "2",
+        org: "MacTech Nigeria",
+        title: "Junior Web Designer",
+        date: "Dec 2023 - Feb 2024",
+        description:  "Built responsive design layouts for German clients, created custom interactions and animations, enhancing user engagement. Collaboratively plan with global teams for design and feature updates and ensured end-to-end product's quality checks, and timely delivery."
+      },  
+    {
+        id: "3",
+        org: "Jowoweb Technologies",
+        title: "R&D Programmer",
+        date: "Jan 2023 - Feb 2024",
+        description:  
+            "I was responsible for the programming of data to be entered on the system built with Microsoft ASP.NET framework. I conducted thorough research and analysis to identify areas for innovation and improvement. Collaborated effectively with cross functional teams to drive the enhancement of the system and user experience.",
+      },   
+    {
+        id: "4",
+        org: "Freelance",
+        title: "ReactJS/NextJS Developer",
+        date: "Since 2021",
+        description:  
+            "I have successfully worked in MERN stack and Nextjs gaining expertise in both frontend and backend development. I have also actively contributed to the testing of Node applications, ensuring high-quality deliverables. Utilizing Agile methodology, I effectively collaborate with teams to enhance productivity and ensure seamless project execution.",
+      },  
+      {
+        id: "5",
+        org: "Freelance",
+        title: "Graphic Designer",
+        date: "2018 - 2020",
+        description:  
+            "As a freelance graphic designer, I have worked independently and collaborated with clients to create visually appealing and impactful designs ideas, and deliver high-quality visual solutions that meet their needs. I have managed my schedule while ensuring timely project delivery and maintaining client satisfaction. ",
+      },  
+    
+];
+
+export default expData;
